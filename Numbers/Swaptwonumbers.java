a1bb0812286e95cf6b82de43914181c95a95ac41
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-06-23 10:28:01 +0530

    swap of 2 numbers

---
+++ Numbers/Swaptwonumbers.java
@@ -0,0 +1,14 @@
+package Numbers;
+import java.util.*;
+public class Swaptwonumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int temp=a;
+        a=b;
+        b=temp;
+    
+    System.out.println(a+"\n"+b);
+    }
+}
