be97247f9bcc665ace5839ff391d9a000569115b
Manneri Harshitha <harshitha.manneri@gmail.com>
2026-06-25 10:31:32 +0530

    Vowel or consonant

---
+++ Strings/Vowelorconsonant.java
@@ -0,0 +1,15 @@
+package Strings;
+import java.util.*;
+
+public class Vowelorconsonant {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch=sc.next().toLowerCase().charAt(0);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            System.out.println("Vowel");
+        }else{
+            System.out.println("Consonant");
+        }
+    }
+    
+}
